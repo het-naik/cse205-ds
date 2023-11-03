24737022e1493b88201ce2a0522bafc5a747a5f6
Het Naik <het.n@ahduni.edu.in>
2023-11-03 18:32:14 +0530

    Lab8

---
+++ assignments/30.10.2023/1464.c
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        priority_queue<int> pq;
+        for (int i:nums){
+            pq.push(i);
+        }
+        int x=pq.top();
+        pq.pop();
+        int y=pq.top();
+        return (x-1)*(y-1);
+    }
+};
